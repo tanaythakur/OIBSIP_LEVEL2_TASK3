c97085342630d60f971994bc30c77510cf58e498
Tanay Singh Thakur <126644413+tanaythakur@users.noreply.github.com>
2023-11-02 00:22:04 +0530

    Add files via upload

---
+++ script00.js
@@ -0,0 +1,26 @@
+const inputBox=document.getElementById("input-box");
+const listcontainer=document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write soomething!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    } 
+    inputBox.value = "";
+}
+
+listConatiner.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false);
